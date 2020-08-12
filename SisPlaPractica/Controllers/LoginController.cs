5390c0d298cf47d47758cc68b9629b383d5d4e92
Willy <willy@correomodificado.com>
2020-08-11 23:30:52 -0500

    Subida inicial

---
+++ SisPlaPractica/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SisPlaPractica.BLL2;
+
+namespace SisPlaPractica.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public IActionResult Get(string email,string clave)
+        {
+            try
+            {
+                return Ok(SisPlaBs.ObtenerUsuario(email, clave));
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
